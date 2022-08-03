f0c535cf8f9f884654a88a13b695a2e76dc2c8bb
UntamedLotus <ruttikajadhav@gmail.com>
2022-08-03 19:44:44 +0530

    Error fixed

--- src/components/mainContent/input/icon/icon.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { IoCheckmarkSharp } from "react-icons/io5";
-
-const Icon = () => {
-  return <IoCheckmarkSharp color={"#000"} />;
-};
-
-export default Icon;
--- src/components/mainContent/mainContent.jsx
+++ src/components/mainContent/mainContent.jsx
@@ -1,40 +1,86 @@
-// import "./mainContent.style.scss";
-
 import React from "react";
-import Input from "./input/input";
-import Screen from "./screen/screen";
+import { IoCheckmarkSharp } from "react-icons/io5";
 
 const MainContent = () => {
-  const [elem, setElem] = React.useState({
-    englishName: "",
-    bodyType: "",
+  const [elems, setElems] = React.useState({
+    result: {},
+    query: "",
+    status: false,
   });
 
   const [body, setBody] = React.useState("");
 
   const handleValue = function (e) {
-    setBody(e.target.value);
+    const val = e.target.value;
+    console.log(val);
+    setBody(val);
   };
 
   const onSubmit = function (e) {
     e.preventDefault();
+    setElems({
+      ...elems,
+      query: body,
+      status: true,
+    });
     setBody("");
-    setElem(body);
   };
 
   React.useEffect(() => {
-    fetch(`https://api.le-systeme-solaire.net/rest/bodies/${elem}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setElem(data);
-        console.log(data);
-      });
-  }, [elem]);
+    elems.status &&
+      fetch(`https://api.le-systeme-solaire.net/rest/bodies/${elems.query}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setElems({
+            result: data,
+          });
+          console.log(data);
+        });
+  }, [elems.status]);
+
+  const pageIntro = function () {
+    return (
+      <div className="page-intro">
+        <h1 className="pageintro-title">Welkin</h1>
+        <p className="pageintro-text">
+          Enter the name of the Celestial body of solar sstem and get basic
+          information about it!
+        </p>
+        <p className="text">
+          This app is made by using <span className="underlined">React</span>.
+        </p>
+      </div>
+    );
+  };
+
+  const Information = function () {
+    return (
+      <div className="">
+        <h2>Basic Properties:</h2>
+        <li>
+          <ul>Name:{elems.result.name}</ul>
+          <ul>Body Type:{elems.result.bodyType}</ul>
+        </li>
+      </div>
+    );
+  };
 
   return (
     <div>
-      <Input body={body} onSubmit={onSubmit} handleValue={handleValue} />
-      <Screen elem={elem} />
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          onChange={handleValue}
+          name="body"
+          value={body}
+          placeholder="Enter name of Celestial body"
+          required
+        />
+        <button>
+          <IoCheckmarkSharp color={"#000"} />
+        </button>
+      </form>
+      {elems.status === false ? pageIntro() : Information()}
     </div>
   );
 };
